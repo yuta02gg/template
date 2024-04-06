104f4c26e886593ebafeda008b2ccf2eb135e0ff
Your Name <you@example.com>
2024-04-06 09:33:57 +0900

    5-2

---
+++ src/com/diworksdev/template/action/BuyItemAction.java
@@ -0,0 +1,64 @@
+package com.diworksdev.template.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuyItemAction extends ActionSupport implements SessionAware {
+    private int stock;
+    private String pay;
+    private Map<String, Object> session;
+    private String result;
+
+    public String execute() {
+        result = SUCCESS;
+
+        // セッションに在庫数を保存
+        session.put("stock", stock);
+
+        // 在庫数と商品価格を計算してセッションに保存
+        int intStock = Integer.parseInt(session.get("stock").toString());
+        int intPrice = Integer.parseInt(session.get("buyItem_price").toString());
+        session.put("buyItem_price", intStock * intPrice);
+
+        // 支払い方法をセッションに保存
+        String payment;
+        if (pay.equals("1")) {
+            payment = "現金払い";
+            session.put("pay", payment);
+        } else {
+            payment = "クレジットカード";
+            session.put("pay", payment);
+        }
+
+        return result;
+    }
+
+    // GetterとSetterメソッド
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getPay() {
+        return pay;
+    }
+
+    public void setPay(String pay) {
+        this.pay = pay;
+    }
+
+    public Map<String, Object> getSession() {
+        return session;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+}
